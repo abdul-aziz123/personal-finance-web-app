9c22e95551ff77d20583f40f9de846d3b1e08ee6
abdul-aziz123 <k224933@nu.edu.pk>
2024-10-11 19:55:03 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import Button from "./components/button";
+import Button from "@/components/button";
+import Tab from "@/components/tab";
+import { IconCaretRight, IconCaretLeft } from "@/icons";
 
 export default function page() {
   return (
     <>
-      <div className="min-h-screen flex justify-center items-center gap-space200">
-        <Button variant="secondary">Placeholder</Button>
+      <div className="flex min-h-screen items-center justify-center gap-space200">
+        <Button variant="page">
+          <IconCaretLeft className="fill-Grey900 group-hover:fill-white" />
+          Prev
+        </Button>
+
+        <Button
+          variant="page"
+          className="size-space500 active:bg-Grey900 active:text-white"
+        >
+          1
+        </Button>
+        <Button variant="page">
+          Next
+          <IconCaretRight className="fill-Grey900 group-hover:fill-white" />
+        </Button>
       </div>
     </>
   );
